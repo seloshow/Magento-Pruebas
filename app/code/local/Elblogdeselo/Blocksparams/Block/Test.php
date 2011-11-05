5111943f851d1fc3ab379644848b8fb463599525
David <david.selo@gmail.com>
2011-11-05 20:30:22 +0100

    probando a pasarle parametros a los bloques

---
+++ app/code/local/Elblogdeselo/Blocksparams/Block/Test.php
@@ -0,0 +1,10 @@
+<?php
+class Elblogdeselo_Blocksparams_Block_Test extends Mage_Core_Block_Abstract{
+	/**
+	protected function _toHtml(){
+		$nombre=$this->getNombre();
+		$apellidos=$this->getApellidos();
+		$html=$nombre." ".$apellidos;
+		return $html;
+	}*/
+}
