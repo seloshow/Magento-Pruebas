f343968059ab85ac0d7b1bdf19e0b8a9b9fe43c0
David <david.selo@gmail.com>
2012-03-23 22:34:20 +0100

    avances en el libro de alanStorm

--- app/code/local/Nofrills/Booklayout/controllers/UpdateController.php
+++ app/code/local/Nofrills/Booklayout/controllers/UpdateController.php
@@ -13,12 +13,24 @@ class Nofrills_Booklayout_UpdateController extends Mage_Core_Controller_Front_Ac
 	}
 	
 public function complexAction()
-		{		
+		{	/*Capitulo anterior al 2.4*/
+			/*	
 			$layout = Mage::getSingleton('core/layout');
 			$path	= Mage::getModuleDir('', 'Nofrills_Booklayout') . DS . 'page-layouts' . DS . 'complex.xml';
 			$xml = simplexml_load_file($path,Mage::getConfig()->getModelClassName('core/layout_element'));
 			$layout->setXml($xml);
 			$layout->generateBlocks();
-			echo $layout->setDirectOutput(true)->getOutput();								
+			echo $layout->setDirectOutput(true)->getOutput();	*/
+			
+			/*seccion 2.6 */
+			$layout = Mage::getSingleton('core/layout');
+			$path = Mage::getModuleDir('','Nofrills_Booklayout').DS.'page-layouts'.DS.'complex.xml';
+			$xml = simplexml_load_file($path ,Mage::getConfig()->getModelClassName('core/layout_element'));
+			$layout->setXml($xml);
+			$text=$layout->createBlock('core/text','foxxy')
+					->setText("The quick brown fox jumped over the lazy dog.");
+			$layout->generateBlocks();
+			$layout->getBlock('content')->insert($text);
+			echo $layout->setDirectOutput(true)->getOutput();
 		}	
 }
